a6d7715f42e3790d759fdd8e6330fb5aed524ad5
Duran <sakalli.duran@gmail.com>
2021-10-21 12:22:27 -0700

    webflux reactive

---
+++ spring-reactive/src/main/java/com/drn/reactive/repo/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.drn.reactive.repo;
+
+import com.drn.reactive.dto.ProductDto;
+import com.drn.reactive.entity.Product;
+import org.springframework.data.domain.Range;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface ProductRepository extends ReactiveMongoRepository<Product, String> {
+
+    Flux<ProductDto> findByPriceBetween(Range<Double> priceRange);
+}
